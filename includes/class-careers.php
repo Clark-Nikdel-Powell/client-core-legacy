448c0f10f9d9a7cbc3c741f157a9b43a2b13cc08
Glenn Welser <glenn@clarknikdelpowell.com>
2020-01-27 14:19:59 -0500

    add setting of labels for completeness

--- includes/class-careers.php
+++ includes/class-careers.php
@@ -35,6 +35,36 @@ class Careers extends Post_Type {
 		$this->publicly_queryable  = false;
 		$this->capability_type     = 'page';
 
+		$this->labels = array(
+			'name'                  => _x( 'Careers', 'Post Type General Name', 'cnp-core' ),
+			'singular_name'         => _x( 'Career', 'Post Type Singular Name', 'cnp-core' ),
+			'menu_name'             => __( 'Careers', 'cnp-core' ),
+			'name_admin_bar'        => __( 'Career', 'cnp-core' ),
+			'archives'              => __( 'Career Archives', 'cnp-core' ),
+			'attributes'            => __( 'Career Attributes', 'cnp-core' ),
+			'parent_item_colon'     => __( 'Parent Career:', 'cnp-core' ),
+			'all_items'             => __( 'WOW! Careers', 'cnp-core' ),
+			'add_new_item'          => __( 'Add New Career', 'cnp-core' ),
+			'add_new'               => __( 'Add New', 'cnp-core' ),
+			'new_item'              => __( 'New Career', 'cnp-core' ),
+			'edit_item'             => __( 'Edit Career', 'cnp-core' ),
+			'update_item'           => __( 'Update Career', 'cnp-core' ),
+			'view_item'             => __( 'View Career', 'cnp-core' ),
+			'view_items'            => __( 'View Careers', 'cnp-core' ),
+			'search_items'          => __( 'Search Careers', 'cnp-core' ),
+			'not_found'             => __( 'Not found', 'cnp-core' ),
+			'not_found_in_trash'    => __( 'Not found in Trash', 'cnp-core' ),
+			'featured_image'        => __( 'Cover Image', 'cnp-core' ),
+			'set_featured_image'    => __( 'Set cover image', 'cnp-core' ),
+			'remove_featured_image' => __( 'Remove cover image', 'cnp-core' ),
+			'use_featured_image'    => __( 'Use as cover image', 'cnp-core' ),
+			'insert_into_item'      => __( 'Insert into career', 'cnp-core' ),
+			'uploaded_to_this_item' => __( 'Uploaded to this career', 'cnp-core' ),
+			'items_list'            => __( 'Career list', 'cnp-core' ),
+			'items_list_navigation' => __( 'Career list navigation', 'cnp-core' ),
+			'filter_items_list'     => __( 'Filter career list', 'cnp-core' ),
+		);
+
 		parent::__construct();
 
 	}
